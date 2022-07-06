05de37fb1eb0bfe880e8d4d0e4c42211960c9a36
Muhammad Rosyad Syamil <42128450+mrcad22@users.noreply.github.com>
2022-07-06 18:54:30 +0700

    web/show.js
    update show

--- src/pages/web/places/Show.js
+++ src/pages/web/places/Show.js
@@ -126,10 +126,62 @@ function WebPlaceShow() {
                         <i className="fa fa-map-marker"></i> <i>{place.address}</i>
                       </span>
                       <hr />
-                      <ImageGallery items={images} autoPlay={true}/>
                       <div
                         dangerouslySetInnerHTML={{ __html: place.description }}
                       />
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-5 mb-4">
+                  <div className="card border-0 rounded shadow-sm">
+                    <div className="card-body">
+                      <h5>
+                        <i className="fa fa-map-marked-alt"></i> MAPS
+                      </h5>
+                      <hr />
+                    </div>
+                    <hr />
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-map-marker-alt"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                            <div className="capt-info fw-bold">ADDRESS</div>
+                            <div className="sub-title-info"><i>{place.address}</i></div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-clock"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">OFFICE HOURS</div>
+                          <div className="sub-title-info">{place.office_hours}</div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-phone"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">PHONE</div>
+                          <div className="sub-title-info">{place.phone}</div>
+                        </div>
+                        <div className="col-md-2 col-2">
+                          <div className="icon-info-green">
+                            <i className="fa fa-globe-asia"></i>
+                          </div>
+                        </div>
+                        <div className="col-md-10 col-10">
+                          <div className="capt-info fw-bold">WEBSITE</div>
+                          <div className="sub-title-info">
+                            <a href={place.website} className="text-decoration-none">{place.website}</a>
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
