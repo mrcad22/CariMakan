972b07c370e887d0710c2c74b7c606c3c669db6b
Muhammad Rosyad Syamil <42128450+mrcad22@users.noreply.github.com>
2022-08-04 11:47:52 +0700

    Update CardCategory.js

--- src/components/utilities/CardCategory.js
+++ src/components/utilities/CardCategory.js
@@ -6,7 +6,7 @@ function CardCategory(props) {
     return (
         <div className="col-6 col-md-3 mb-4" key={props.id}>
             <Link to={`/category/${props.slug}`} className="text-decoration-none text-dark">
-                <div className="card h-100 border-0 rounded shadow-sm">
+                <div className="card h-100 w-100 border-0 rounded shadow-sm">
                     <div className="card-body text-center">
                         <img src={props.image} style={{ width: "80px" }} alt=""/>
                         <hr/>
